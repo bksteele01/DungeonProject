c14375c9c57f81c69ff6f169debf864bff50b1f8
Ben <bksteele01@gmail.com>
2021-10-27 22:06:25 +0000

    added Item class

---
+++ Item.java
@@ -0,0 +1,59 @@
+class Item{
+
+        //this class holds information for an item object. It can build an item object, and contains methods
+        //to return certain elements of the item.
+
+        private ItemType type;
+        private String name;
+        private int weight;
+        private int value;
+        private int strength;
+
+        Item(ItemType type, String name, int weight, int value, int strength){
+
+                //this is a constructor for an item object. it takes all the parameters and sets them equal
+                //to the instance variables of the object.
+
+                this.type = type;
+                this.name = name;
+                this.weight = weight;
+                this.value = value;
+                this.strength = strength;
+        }
+
+        public ItemType getType(){
+
+                //this method returns the instance variable type.
+
+                return type;
+        }
+
+        public int getWeight(){
+
+                //tbis method returns the instance variable weight.
+
+                return weight;
+        }
+
+        public int getValue(){
+
+                //this method returns the instance variable value.
+
+                return value;
+        }
+	public String getName(){
+
+                //this method returns the instance variable name.
+
+                return name;
+        }
+
+        public String toString(){
+
+                //this method returns all of the data that the object contains in an informative format.
+
+                return("Type: "+type+" --- Name: "+name+" --- Weight: "+weight+" --- Value: "+value+" --- Strength: "+strength);
+        }
+}
+
+
