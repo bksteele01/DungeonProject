3e4d4c1bdf01a0e0f17113bde57e0531ecded2a7
Josh <jcottrel@mail.umw.edu>
2021-11-01 18:10:24 +0000

    Added movement to the enemy class

--- Enemy.java
+++ Enemy.java
@@ -7,15 +7,27 @@ class Enemy {
 	int maxHealth;
 	int currHealth;
 	int damage;
+	int x;
+	int y;
 
 
 
-	public enemy (int currHealth, int maxHealth, int damage, String name) {
+	public enemy (int currHealth, int maxHealth, int damage, String name, int x, int y) {
 		this.maxHealth = maxHealth;
 		this.damage = damage;
 		this.name = name;
 		this.currHealth = currHeatlh;
+		this.x = x;
+		this.y = y;
 
+	}
+
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
 	}
 
 	public String getName() {
@@ -46,4 +58,8 @@ class Enemy {
 		currHealth = newCurrHealth;
 	}
 
+	public void moveLocation(int newX, int newY) {
+		x = newX;
+		y = newY;
+	}
 }
