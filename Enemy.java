bf836afff4ee8e6e78a46ac6b4085087a373fb8a
Josh <jcottrel@mail.umw.edu>
2021-11-04 23:54:49 +0000

    Added comments to player and enemy classes

--- Enemy.java
+++ Enemy.java
@@ -1,7 +1,9 @@
 
-
+//Creates the enemy class which can create an enemy object and has methods for setting and retrieving information about the enemy object
 class Enemy {
 
+
+	//Initializes variables
 
 	String name;
 	int maxHealth;
@@ -11,7 +13,8 @@ class Enemy {
 	int y;
 	boolean isDead;
 
-
+	//Construsts an enemy object based on the passes parameters of current health, max health, damage, name, and x and y coordinates
+	//then sets variables accordingly
 
 	public Enemy (int currHealth, int maxHealth, int damage, String name, int x, int y) {
 		this.maxHealth = maxHealth;
@@ -24,49 +27,74 @@ class Enemy {
 
 	}
 
+	//returns the enemy's x coordinate
+
 	public int getX() {
 		return x;
 	}
+
+	//returns the enemy's y coordinate
 	
 	public int getY() {
 		return y;
 	}
 
+	//returns the enemy's name
+
 	public String getName() {
 		return name;
 	}
+
+	//returns the enemy's max health
 
 	public int getMaxHealth() {
 		return maxHealth;
 	}
 
+	//returns the enemy's current health
+
 	public int getCurrHealth() {
 		return currHealth;
 	}
+
+	//returns the enemy's damage
 
 	public int getDamage() {
 		return damage;
 	}
 
+	//changes the enemy's name to the new name given when the method was called
+
 	public void setName(String newName) {
 		name = newName;
 	}
+
+	//changes the enemy's max health to the new max health given when the method was called
 
 	public void setMaxHealth(int newMaxHealth) {
 		maxHealth = newMaxHealth;
 	}
 
+	//changges the enemy's current health to the new current health given when the method was called
+
 	public void setCurrent(int newCurrHealth) {
 		currHealth = newCurrHealth;
 	}
+
+	//changes the enemy's x and y coordinates to the new coordinates given when the method was called
 
 	public void moveLocation(int newX, int newY) {
 		x = newX;
 		y = newY;
 	}
+
+	//checks if the enemy is dead or alive
+
 	public boolean getIsDead(){
 		return isDead;
 	}
+
+	//kills the enemy and returns that the enemy is dead
 	
 	public boolean setIsDead() {
 		isDead = true;
--- Player.java
+++ Player.java
@@ -1,10 +1,13 @@
 import java.util.*;
 import ansi_terminal.*;
 
-
+//creates the player class which can construst a player object and has methods to get and set all of the players variables as well as initiate a battle with an enemy
 public class Player {
+
+	//Initializes variables and the players starting items
+
 	private String name;
-	private Inventory playerInv = new Inventory(100); //I don't know what weight is good for your inventory system, if 15 is too low/large feel free to change it
+	private Inventory playerInv = new Inventory(100);
 	private int currentHealth;
 	private int maxHealth;
 	private int xCord;
@@ -13,6 +16,11 @@ public class Player {
 	Item IronSword = new Item(weapon, "Iron Sword", 8, 50, 10);
 	ItemType armor = ItemType.armor;
 	Item woodenChestplate = new Item(armor, "Wooden Chestplate", 5, 60, 10);
+
+	//This is the player constructor, which takes the name of the player and their current health as parameters.
+	//After creating the player object the constructor sets the players name, coordinates, inventory, items, and health
+	//and then prints out the players name and health
+
 	public Player(String name, int currentHealth) {
 		System.out.print("\n \r");
 		xCord = 4;
@@ -29,47 +37,76 @@ public class Player {
 
 	}
 
+	//returns the name of the player when called
+
 	public String getName() {
 		return name;
 	}
+
+	//returns the inventory of the player when called
 
 	public Inventory getInv() {
 		return playerInv;
 	}
 
+	//when called with the players new name as parameters, this method will change the players name to that new name
+
 	public void setName(String newName) {
 		name = newName;
 	}
 
+	//sets the players x coordinate to the x coordinate that was given when the method was called
+
 	public void setxCord(int x){
 		xCord = x;
 	}
+
+	//sets the players y coordinate to the y coordinate that was given when the method was called
+
 	public void setyCord(int y){
 		yCord = y;
 	}
+
+	//sets the players max health to the integer that was given when the method was called
 
 	public void setMaxHealth(int newMax) {
 		maxHealth = newMax;
 	}
 
+	//sets the players current health to the integer that was given when the method was called
+
 	public void setCurrentHealth(int newCurr) {
 		currentHealth = newCurr;
 	} 
 
+	//returns the players current health when called
+
 	public int getCurrentHealth() {
 		return currentHealth;
 	}
-
+	
+	//returns the players x coordinate when called
 
 	public int getXcord() {
 		return xCord;
 
 	}
 
+	//reutrns the players y coordinate when called
+
 	public int getYcord() {
 		return yCord;
 	}
 
+	//Initiates a battle between the player and the enemy given when the method was called.
+	//First, the method finds what items the player has equipped and then gets their strength values
+	//Next,the method finds the enemy's damage values and initializes the final damage integer
+	//Next, the method checks if the enemy's damage is greater than the players defense. If the enemy's damage is less
+	//than or equal to the players defense, the damage will be 0. If the damage is greater than the players defense the 
+	//damage will be equal to the players defense minus the enemies damage
+	//Finally, the method sets the player and enemy's updates health values and prints the outcome of the battle
+	//It also checks to see if the enemy's health is less than or equal to zero. If it is, it prints that the enemy is dead
+	//and returns true, otherwise it returns false.
 
 	public boolean Battle(Enemy enemy) {
 		Item weapon = playerInv.getWeapon();
